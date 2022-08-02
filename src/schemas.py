0fd36db27beaf83e9b40707cd76dca16a9dd9399
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-02 20:58:54 +0300

    [ADD] validators for schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,20 +1,47 @@
-from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+from pydantic import BaseModel, Field, validator
 
 
 class BodyUser(BaseModel):
-    username: str
-    password: str
-    lastName: str
-    firstName: str
+    username: str = Field(description="Username", max_length=50)
+    password: str = Field(description="Password", max_length=50)
+    firstName: Optional[str] = Field(description="First name", max_length=50, default=None)
+    lastName: Optional[str] = Field(description="Last name", max_length=50, default=None)
+
+    @validator("username")
+    def valid_username(cls, username: str):
+        pass
+
+    @validator("firstName")
+    def valid_first_name(cls, first_name: str):
+        pass
+
+    @validator("lastName")
+    def valid_last_name(cls, last_name: str):
+        pass
 
 
-class BodyPost(BaseModel):
-    title: str
-    text: str
-    authorId: int
+class BodyCreatePost(BaseModel):
+    title: Optional[str] = Field(description="Post title", max_length=50, default=None)
+    text: Optional[str] = Field(description="Post text", default=None)
+    authorId: int = Field(description="Post author")
+
+    @validator("authorId")
+    def valid_author_id(cls, author_id: int):
+        pass
+
+    @validator("title")
+    def valid_title_id(cls, title: str):
+        pass
+
+    @validator("text")
+    def valid_text_id(cls, text: str):
+        pass
 
 
-class BodyComment(BaseModel):
+class BodyCreateComment(BaseModel):
     text: str
     parentId: int
     postId: int
