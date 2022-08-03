7edba7dd880b16cd56924b5aa9e9c8f4c059317d
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-03 14:26:48 +0300

    [ADD] post endpoints [FIX] user endpoints [ADD] new schemas

--- src/endpoints/post.py
+++ src/endpoints/post.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
+
+from ..auth import JWTBearer, AutoHandler
+from ..models import PostModel
+from ..base.schemas import BodyCreatePost, ResponseStatus
 
 
 router = APIRouter(
@@ -8,7 +12,17 @@ router = APIRouter(
 
 
 @router.post(
-    "/"
+    "/",
+    dependencies=[Depends(JWTBearer())],
+    response_model=ResponseStatus
 )
-async def create_post():
-    pass
+async def create_post(request: Request, body: BodyCreatePost):
+    """
+    Create new post
+
+    - **title**: post title (optional)
+    - **text**: post text (optional)
+    """
+    body.authorId = AutoHandler.decode_jwt_token(request.headers.get("Authorization").split(" ")[1]).get("userId")
+    print(body)
+    # return ResponseStatus(status=PostModel.create(data=body))
