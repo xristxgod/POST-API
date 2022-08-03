681d11c21412403c4ddc30199c33e2c8392ca550
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-03 14:51:45 +0300

    [ADD] update&delete routes

--- src/endpoints/post.py
+++ src/endpoints/post.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter, Depends, Request
 
 from ..auth import JWTBearer, AutoHandler
 from ..models import PostModel
-from ..base.schemas import BodyCreatePost, ResponseStatus
+from ..base.schemas import BodyModPost, DataCreatePost, QueryPost, ResponsePost, ResponseStatus
 
 
 router = APIRouter(
     prefix="/post",
-    tags=["POST"]
+    tags=["Post"]
 )
 
 
@@ -16,13 +16,56 @@ router = APIRouter(
     dependencies=[Depends(JWTBearer())],
     response_model=ResponseStatus
 )
-async def create_post(request: Request, body: BodyCreatePost):
+async def create_post(request: Request, body: BodyModPost):
     """
     Create new post
 
     - **title**: post title (optional)
     - **text**: post text (optional)
     """
-    body.authorId = AutoHandler.decode_jwt_token(request.headers.get("Authorization").split(" ")[1]).get("userId")
-    print(body)
-    # return ResponseStatus(status=PostModel.create(data=body))
+    return ResponseStatus(status=PostModel.create(data=DataCreatePost(
+        title=body.title,
+        text=body.text,
+        authorId=AutoHandler.decode_jwt_token(request.headers.get("Authorization").split(" ")[1]).get("userId")
+    )))
+
+
+@router.get(
+    "/",
+    response_model=ResponsePost
+)
+async def get_post(query: QueryPost = Depends()):
+    """
+    Get post by id
+
+    - **postId**: post id
+    """
+    return PostModel.read(query.postId)
+
+
+@router.put(
+    "/",
+    dependencies=[Depends(JWTBearer())],
+    response_model=ResponseStatus
+)
+async def update_post(request: Request, body: BodyModPost, query: QueryPost = Depends()):
+    """
+    Update post
+
+    - **title**: post title (optional)
+    - **text**: post text (optional)
+    """
+
+
+@router.delete(
+    "/",
+    dependencies=[Depends(JWTBearer())],
+    response_model=ResponsePost
+)
+async def get_post(query: QueryPost = Depends()):
+    """
+    Delete post by id
+
+    - **postId**: post id
+    """
+    # return PostModel.read(query.postId)
