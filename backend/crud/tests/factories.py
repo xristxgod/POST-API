d29fa7b280c077f25c360907282e93b76596a27a
Mamedov Murad <mamedov_99b@mail.ru>
2022-11-11 15:43:21 +0300

    [ADD] Fake all models

--- backend/crud/tests/factories.py
+++ backend/crud/tests/factories.py
@@ -21,9 +21,45 @@ class FakeUser(AbsFakeModel):
     model = models.User
 
     @classmethod
-    async def create(cls, active: int = True) -> models.User:
+    async def create(cls, **params: Optional) -> models.User:
         return await cls.model.create(
-            username='@' + fake.unique.name().lower().replace(' ', '_'),
+            username='@' + fake.unique.user_name(),
             password_hash=fake.unique.password,
-            active=active
+            active=params.get('active', True)
+        )
+
+
+class FakePost(AbsFakeModel):
+    model = models.Post
+
+    @classmethod
+    async def create(cls, **params: Optional) -> MODEL:
+        return await cls.model.create(
+            title=fake.unique.name(),
+            text=fake.unique.text(),
+            video_link=fake.unique.uri()
+        )
+
+
+class FakeComment(AbsFakeModel):
+    model = models.Comment
+
+    @classmethod
+    async def create(cls, **params: Optional) -> MODEL:
+        return await cls.model.create(
+            text=fake.unique.text()
+        )
+
+
+class FakeImage(AbsFakeModel):
+    model = models.Image
+
+    @classmethod
+    async def create(cls, **params: Optional) -> MODEL:
+        return await cls.model.create(
+            main=params.get('main', False),
+            image=fake.image(size=(2, 2), hue='purple', luminosity='bright', image_format='ico'),
+            user=params.get('user'),
+            post=params.get('post'),
+            comment=params.get('comment')
         )
